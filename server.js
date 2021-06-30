eef4aef2e4a9fb230f95d0318ab4f69988d06dd4
MykalB <mykalb081@gmail.com>
2021-06-30 16:22:10 -0400

    Security
    - Use helmet.js to ensure protection from application exploits by only allowing scripts from server.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const helmet      = require('helmet');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -16,6 +17,21 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Security features.
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: { 
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"] 
+    }
+  },
+  dnsPrefetchControl: false
+}));
 
 //Index page (static HTML)
 app.route('/')
